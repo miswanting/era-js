bbca3085bd878df2b5eadedbd576a30e7226de2b
何雨航 <miswanting@users.noreply.github.com>
2018-10-12 08:03:48 +0800

    Half

--- src/sematic-ui-react/PageList.tsx
+++ src/sematic-ui-react/PageList.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Page from "./Page";
+import { Grid, GridRow } from 'semantic-ui-react'
 /**
  * 页面列表（较长）
  */
@@ -12,11 +13,15 @@ export default class PageList extends React.Component<{ data: any }, {}> {
     render() {
         let pages = this.props.data.pages.map((page: any, index: number) => {
             if (index == this.props.data.pages.length - 1) {
-                return <Page key={index} data={page} isDisabled={false} />
+                return <GridRow style={{ paddingTop: 0 + 'px' }}>
+                    <Page key={index} data={page} isDisabled={false} />
+                </GridRow>
             } else {
-                return <Page key={index} data={page} isDisabled={true} />
+                return <GridRow style={{ paddingTop: 0 + 'px' }}>
+                    <Page key={index} data={page} isDisabled={true} />
+                </GridRow>
             }
         })
-        return <div style={{ overflowY: 'auto' }}>{pages}</div>
+        return <Grid style={{ height: 100 + '%', width: 100 + '%', margin: 0 + 'px' }}>{pages}</Grid>
     }
 }

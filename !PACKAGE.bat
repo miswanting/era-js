59e00c593d76ca947bea4f7895e680f81fe8abda
何雨航 <miswanting@users.noreply.github.com>
2018-10-16 00:28:33 +0800

    Package

--- !PACKAGE.bat
+++ !PACKAGE.bat
@@ -1,5 +1,7 @@
 @echo off
-call .\node_modules\.bin\electron-packager --overwrite .
+REM call .\node_modules\.bin\electron-packager --overwrite .
+call .\node_modules\.bin\electron-builder --dir
+REM call .\node_modules\.bin\electron-builder
 mkdir .\build\Era.js
 xcopy .\era.js-win32-x64\* .\build\Era.js\ /e /y
 rmdir /s /q .\era.js-win32-x64

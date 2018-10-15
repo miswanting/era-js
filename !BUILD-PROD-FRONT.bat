823c736a38c88f6c7272587d55c0c10310013c0e
何雨航 <miswanting@users.noreply.github.com>
2018-10-16 00:40:00 +0800

    Fix

--- !BUILD-PROD-FRONT.bat
+++ !BUILD-PROD-FRONT.bat
@@ -2,6 +2,6 @@
 title Main
 chcp 65001
 cls
-call ./node_modules/.bin/webpack  --config webpack.dev1.js
+call ./node_modules/.bin/webpack  --config webpack.prod.js
 call ./node_modules/.bin/electron .
 pause

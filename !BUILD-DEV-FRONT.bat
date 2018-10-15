8324ec7559925a308f62f9aa93bfc364562bd68c
何雨航 <miswanting@users.noreply.github.com>
2018-10-16 00:13:09 +0800

    生产模式实装

--- !BUILD-DEV-FRONT.bat
+++ !BUILD-DEV-FRONT.bat
@@ -2,6 +2,6 @@
 title Main
 chcp 65001
 cls
-call ./node_modules/.bin/webpack --config webpack.dev.js
+call ./node_modules/.bin/webpack
 call ./node_modules/.bin/electron .
 pause
--- !DEBUG-DEV-FRONT.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Main
-chcp 65001
-cls
-call ./node_modules/.bin/electron .
-pause

266043026de654a489b8d91bd022b40edfcdcb1e
何雨航 <453542772@qq.com>
2018-11-07 13:16:00 +0800

    新增目录

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,8 @@ function createWindow() {
     win = new BrowserWindow({ width: 1024, height: 768 })
     win.loadFile('src/index.html')
     // win.webContents.openDevTools() // 生产环境下请注释掉
-    Menu.setApplicationMenu(null)
+    var menu = Menu.buildFromTemplate(menu_bar)
+    Menu.setApplicationMenu(menu)
     win.on('closed', () => {
         win = null
         console.log('[DEBG]检测到窗口关闭');
@@ -161,3 +162,88 @@ function data2bag(data: any) {
 function bag2data(bag: any): string {
     return JSON.stringify(bag)
 }
+const menu_bar: any = [
+    {
+        label: '游戏',
+        submenu: [
+            {
+                label: '启动游戏脚本',
+                enabled: false
+            },
+            { type: 'separator' },
+            {
+                label: '退出',
+                enabled: false
+            }
+        ]
+    },
+    {
+        label: '编辑器',
+        submenu: [
+            {
+                label: '头像编辑器',
+                enabled: false
+            }, {
+                label: '地图编辑器',
+                enabled: false
+            }, {
+                label: '图形化代码编辑器',
+                enabled: false
+            }
+        ]
+    }, {
+        label: '调试器',
+        submenu: [
+            {
+                label: '前端调试器',
+                click: function () {
+                    win.webContents.openDevTools()
+                }
+            }
+        ]
+    }, {
+        label: '窗口',
+        role: 'window',
+        submenu: [
+            {
+                label: '放大',
+                role: 'zoomin'
+            }, {
+                label: '缩小',
+                role: 'zoomout'
+            }, {
+                label: '重置',
+                role: 'resetzoom'
+            }
+        ]
+    }, {
+        label: '帮助',
+        role: 'help',
+        submenu: [
+            {
+                label: '文档',
+                enabled: false
+            },
+            {
+                label: '教程',
+                enabled: false
+            },
+            { type: 'separator' },
+            {
+                label: '讨论/建议/反馈Bug',
+                enabled: false
+            },
+            { type: 'separator' },
+            {
+                label: '检查更新',
+                enabled: false
+            },
+            { type: 'separator' },
+            {
+                label: '关于',
+                role: 'about',
+                enabled: false
+            }
+        ]
+    }
+]

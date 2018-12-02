730d6c5b827b952311f21ce9feb5e9336a83ab6d
Miswanting <miswanting@users.noreply.github.com>
2018-12-02 16:26:51 +0800

    修复屏幕清空Bug

--- src/sematic-ui-react/App.tsx
+++ src/sematic-ui-react/App.tsx
@@ -13,9 +13,10 @@ export default class App extends React.Component<{ data: any }, {}> {
     render() {
         if (!this.props.data.isLoaded) {
             return <Xcover data={this.props.data} />
-        } else if (this.props.data.isConsole) {
-            return <div></div>
         }
+        // else if (this.props.data.isConsole) {
+        //     return <div></div>
+        // }
         // return <div style={{ position: 'relative', height: '100%' }}>
         //     <div style={{ position: 'absolute', bottom: 10, left: 10, right: 10 }}>
         //         <PageList data={this.props.data} />

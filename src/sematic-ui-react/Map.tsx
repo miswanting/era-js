11361460acf6ab8204197958a7a040ec5059ab05
何雨航 <453542772@qq.com>
2019-01-03 00:08:19 +0800

    新增地图编辑器2：控制单个Cell

--- src/sematic-ui-react/Map.tsx
+++ src/sematic-ui-react/Map.tsx
@@ -1,27 +1,52 @@
 import * as React from 'react'
 import { Delaunay } from "d3-delaunay"
 import { polygonCentroid } from 'd3'
+import { lookup } from 'dns';
 export default class Map extends React.Component<{ data: any }, {}> {
     constructor(props: any) {
         super(props)
 
     }
     componentDidMount() {
+        // 初始化
         const canvas: any = this.refs.canvas
         const ctx: any = canvas.getContext("2d")
+        const n = 10000
         const points = []
-        const n = 1000
-        for (let i = 0; i < n; i++) {
-            // let x = Math.random() * window.innerWidth
-            // let y = Math.random() * window.innerHeight
-            let x = window.innerWidth / 2 + Math.random()
-            let y = window.innerHeight / 2 + Math.random()
-            points.push([x, y])
-        }
         document.getElementById('root').style.overflowY = 'hidden'
         ctx.canvas.width = window.innerWidth;
         ctx.canvas.height = window.innerHeight;
-        function loop() {
+        // 主流程
+        randomPoints() // 随机点
+        relaxPoints() // 分散点
+        testRender()
+        // drawVoronoi() // 分散点
+        //函数
+        function randomPoints() {
+            for (let i = 0; i < n; i++) {
+                let x = Math.random() * ctx.canvas.width
+                let y = Math.random() * ctx.canvas.height
+                // let x = window.innerWidth / 2 + Math.random()
+                // let y = window.innerHeight / 2 + Math.random()
+                points.push([x, y])
+            }
+        }
+        function relaxPoints() {
+            for (let i = 0; i < 10; i++) {
+                const delaunay = Delaunay.from(points);
+                const voronoi = delaunay.voronoi([0, 0, ctx.canvas.width, ctx.canvas.height]);
+                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+                for (let j = 0; j < n; j++) {
+                    var cvt: [number, number][] = []
+                    for (let k = 0; k < voronoi.cellPolygon(j).length; k++) {
+                        var [x, y] = voronoi.cellPolygon(j)[k]
+                        cvt.push([x, y])
+                    }
+                    [points[j][0], points[j][1]] = polygonCentroid(cvt);
+                }
+            }
+        }
+        function drawVoronoi() {
             const delaunay = Delaunay.from(points);
             const voronoi = delaunay.voronoi([0, 0, ctx.canvas.width, ctx.canvas.height]);
             ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -31,26 +56,33 @@ export default class Map extends React.Component<{ data: any }, {}> {
             ctx.strokeStyle = "red";
             ctx.stroke()
             ctx.beginPath()
-            for (let j = 0; j < n; j++) {
-                var cvt: [number, number][] = []
-                for (let k = 0; k < voronoi.cellPolygon(j).length; k++) {
-                    var [x, y] = voronoi.cellPolygon(j)[k]
-                    cvt.push([x, y])
-                }
-                const [x0, y0] = [points[j][0], points[j][1]];
-                const [x1, y1] = [points[j][0], points[j][1]] = polygonCentroid(cvt);
-                ctx.moveTo(x0, y0)
-                ctx.lineTo(x1, y1)
-            }
             ctx.strokeStyle = "black";
             ctx.stroke();
-            requestAnimationFrame(loop)
         }
-        requestAnimationFrame(loop)
-        // for (let i = 0; i < 500; i++) {
-
+        function testRender() {
+            const delaunay = Delaunay.from(points);
+            const voronoi = delaunay.voronoi([0, 0, ctx.canvas.width, ctx.canvas.height]);
+            ctx.beginPath()
+            voronoi.renderCell(55, ctx)
+            ctx.strokeStyle = "red";
+            ctx.stroke()
+            var l = delaunay.neighbors(55)
+            ctx.beginPath()
+            while (l.next().done == false) {
+                voronoi.renderCell(l.next().value, ctx)
+            }
+            ctx.strokeStyle = "blue";
+            ctx.stroke()
+            console.log();
+        }
+        // var i = 0
+        // function loop() {
+        //     const delaunay = Delaunay.from(points);
+        //     const voronoi = delaunay.voronoi([0, 0, ctx.canvas.width, ctx.canvas.height]);
+        //     voronoi.renderCell(i, ctx)
+        //     requestAnimationFrame(loop)
         // }
-
+        // requestAnimationFrame(loop)
         window.onresize = () => {
             const delaunay = Delaunay.from(points);
             const voronoi = delaunay.voronoi([0, 0, ctx.canvas.width, ctx.canvas.height]);

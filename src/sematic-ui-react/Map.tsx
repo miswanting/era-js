6761eee5bb709d74fd75b893a32cf1703e947310
Miswanting <miswanting@users.noreply.github.com>
2019-05-19 16:39:31 +0800

    显示地图同步

--- src/sematic-ui-react/Map.tsx
+++ src/sematic-ui-react/Map.tsx
@@ -104,41 +104,94 @@ export default class Map extends React.Component<{ data: any }, {}> {
                 var transform = d3.zoomTransform(this);
                 d3.select('#viewport').attr("transform", transform.toString())
             }
+            var new_points = []
+            var node = delaunay.hull
+            for (let i = 0; i < delaunay.points.length / 2; i++) {
+                var point = {
+                    i: i,
+                    x: delaunay.points[2 * i],
+                    y: delaunay.points[2 * i + 1],
+                    h: 0,
+                    n: [],
+                }
+                var rate = simplex.noise2D(point.x * 0.003, point.y * 0.003)
+                rate = (rate + 1) / 2
+                let maxDis = (window.innerWidth + window.innerHeight) / 2
+                let relDis = Math.abs(point.x - window.innerWidth / 2) + Math.abs(point.y - window.innerHeight / 2)
+                let disRate = 100 - (1 - relDis / maxDis) * 99
+                rate = Math.pow(rate, 1 / 10 * disRate)
+                point.h = rate * 10000 - 2000 // [-2000, 8000]
+                var tmp = delaunay.neighbors(i)
+                while (true) {
+                    var n = tmp.next()
+                    if (n.done) { break }
+                    point.n.push(n.value)
+                }
+                new_points.push(point)
+                console.log(node.next);
+            }
+
             d3.select('#root')
                 .append('svg')
+                .attr('id', 'svg')
                 .attr('width', window.innerWidth)
                 .attr('height', window.innerHeight)
                 .call(zoom)
                 .append('g')
                 .attr('id', 'viewport')
-            for (let i = 0; i < delaunay.points.length / 2; i++) {
-                var x = delaunay.points[2 * i]
-                var y = delaunay.points[2 * i + 1]
-                var value = simplex.noise2D(x * 0.003, y * 0.003)
-                value = (value + 1) / 2
-                value = value * value
-                var r = 100, g = 100, b = 100
-                value = parseInt((value * 256).toString())
-                r = g = b = value
-                if (value / 256 > 0.9) { // 白
-                    r = g = b = (value / 256 - 0.9) * 10 * 128 + 128
-                } else if (value / 256 > 0.8) { // 灰
-                    r = g = b = 50
-                } else if (value / 256 > 0.6) { // 深绿
+                .append('g')
+                .attr('id', 'background')
+                .append('rect')
+                .attr('width', window.innerWidth)
+                .attr('height', window.innerHeight)
+                .attr('fill', `rgb(60,81,195)`)
+            d3.select('#viewport')
+                .append('g')
+                .attr('id', 'cells')
+            d3.select('#svg')
+                .append('g')
+                .attr('id', 'ui')
+
+
+            for (let i = 0; i < new_points.length; i++) {
+                // if (value / 256 > 0.9) { // 白
+                //     r = g = b = (value / 256 - 0.9) * 10 * 128 + 128
+                // } else if (value / 256 > 0.8) { // 灰
+                //     r = g = b = 50
+                // } else if (value / 256 > 0.6) { // 深绿
+                //     r = 27, g = 122, b = 34
+                // } else if (value / 256 > 0.4) { // 浅绿
+                //     r = 31, g = 192, b = 42
+                // } else if (value / 256 > 0.2) { // 黄
+                //     r = 243, g = 245, b = 65
+                // } else if (value / 256 > 0.1) { // 浅蓝
+                //     r = 60, g = 81, b = 195
+                // } else { // 深蓝
+                //     r = 25, g = 35, b = 93
+                // }
+                var r, g, b = 0
+                if (new_points[i].h > 6000) { // 白
+                    r = g = b = (new_points[i].h - 6000) / 2000 * 256 / 4 * 3 + 64
+                } else if (new_points[i].h > 5000) { // 灰
+                    r = g = b = 64
+                } else if (new_points[i].h > 4000) { // 深绿
                     r = 27, g = 122, b = 34
-                } else if (value / 256 > 0.4) { // 浅绿
+                } else if (new_points[i].h > 1000) { // 浅绿
                     r = 31, g = 192, b = 42
-                } else if (value / 256 > 0.2) { // 黄
+                } else if (new_points[i].h > 0) { // 黄
                     r = 243, g = 245, b = 65
-                } else if (value / 256 > 0.1) { // 浅蓝
+                } else if (new_points[i].h > -1000) { // 浅蓝
                     r = 60, g = 81, b = 195
                 } else { // 深蓝
                     r = 25, g = 35, b = 93
                 }
-                d3.select('#viewport')
-                    .append('path')
-                    .attr('d', voronoi.renderCell(i))
-                    .attr('fill', `rgb(${r},${g},${b})`)
+                if (new_points[i].h > 0) {
+                    d3.select('#cells')
+                        .append('g')
+                        .append('path')
+                        .attr('d', voronoi.renderCell(i))
+                        .attr('fill', `rgb(${r},${g},${b})`)
+                }
             }
         }
     }

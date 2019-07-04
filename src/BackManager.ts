f5948cee4d9d5bddc48b503a507abf091cdae677
Miswanting <miswanting@users.noreply.github.com>
2019-07-04 19:20:24 +0800

    修复启动后端逻辑

--- src/BackManager.ts
+++ src/BackManager.ts
@@ -7,8 +7,10 @@ export default class BackManager extends EventEmitter {
     public init() {
     }
     public start(exec_file) {
+        console.log(exec_file, fs.existsSync(exec_file))
         if (fs.existsSync(exec_file)) {
-            var game = child_process.execFile(exec_file, { maxBuffer: 512 * 1024 })
+            // var game = child_process.execFile(exec_file, { maxBuffer: 512 * 1024 })
+            child_process.exec(exec_file);
         }
     }
 }

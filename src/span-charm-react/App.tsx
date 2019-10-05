da198ec3b68621660f44223d3776b53c985e2f56
(ง •_•)ง <miswanting@users.noreply.github.com>
2019-10-05 14:53:16 +0800

    Update App.tsx

--- src/span-charm-react/App.tsx
+++ src/span-charm-react/App.tsx
@@ -1,23 +1,31 @@
 import "./span-charm.sass"
 
 import React, { useState } from 'react';
-// import { Splash } from "./Splash"
+import { Splash } from "./Splash"
 import { Header } from "./Header";
-// import { Console } from "./Console"
+import { Console } from "./Console"
 import { Game } from "./Game";
 export default function App(props: any) {
     const [data, setData] = useState(props.data);
     const [style, setStyle] = useState(props.style);
     if (data.isConsole) {
         return (
-            <></>
+            <>
+                <Header data={data} style={{}} />
+                <Console data={data} style={{}} />
+            </>
         )
     } else if (!data.isLoaded) { // Splash界面
-
+        return (
+            <>
+                <Header data={data} style={{}} />
+                <Splash data={data} style={{}} />
+            </>
+        );
     } else {
         return (
             <>
-                <Header data={data.title} style={{}} />
+                <Header data={data} style={{}} />
                 <Game data={data} style={{}} />
             </>
         );

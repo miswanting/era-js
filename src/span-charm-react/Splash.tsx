1d1a6dfee845d2bfe8d4daca544990f03d4a0cf2
(ง •_•)ง <miswanting@users.noreply.github.com>
2019-10-10 21:49:21 +0800

    修复Splash界面显示加载状况的文字功能

--- src/span-charm-react/Splash.tsx
+++ src/span-charm-react/Splash.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 export function Splash(props: any) {
-    return (
-        <div style={{ height: 100 + '%', display: 'flex', alignItems: 'center' }}>
-            <div style={{ width: 100 + '%', textAlign: 'center' }}>
-                <div>{props.data.title}</div>
-                <div>Connecting...</div>
-                <div>按[Esc]键打开菜单</div>
-                <div>按[`]键打开控制台</div>
+    if (props.data.load_text == '') {
+        return (
+            <div style={{ height: 100 + '%', display: 'flex', alignItems: 'center' }}>
+                <div style={{ width: 100 + '%', textAlign: 'center' }}>
+                    <div>{props.data.title}</div>
+                    <div>Connecting...</div>
+                    <div>按[Esc]键打开菜单</div>
+                    <div>按[`]键打开控制台</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div style={{ height: 100 + '%', display: 'flex', alignItems: 'center' }}>
+                <div style={{ width: 100 + '%', textAlign: 'center' }}>
+                    <div>{props.data.title}</div>
+                    <div>Loading...</div>
+                    <div>{props.data.load_text}</div>
+                    <div>按[Esc]键打开菜单</div>
+                    <div>按[`]键打开控制台</div>
+                </div>
+            </div>
+        )
+    }
 }

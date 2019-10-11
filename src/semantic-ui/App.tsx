ea198309b551ef29128a3bf8059ab151345da255
(ง •_•)ง <miswanting@users.noreply.github.com>
2019-10-12 00:06:10 +0800

    新增加载界面Header与Toast

--- src/semantic-ui/App.tsx
+++ src/semantic-ui/App.tsx
@@ -79,3 +79,132 @@ export default function App(props: any) {
         );
     }
 }
+export function Header(props: any) {
+    const [data, setData] = useState(props.data);
+    const [style, setStyle] = useState(props.style);
+    function toggleDevTools() {
+        if (remote.getCurrentWindow().webContents.isDevToolsOpened) {
+            remote.getCurrentWindow().webContents.closeDevTools()
+        } else {
+            remote.getCurrentWindow().webContents.openDevTools();
+        }
+    }
+    function minWindow() {
+        remote.getCurrentWindow().minimize();
+    }
+    function maxWindow() {
+        if (!remote.getCurrentWindow().isMaximized()) {
+            remote.getCurrentWindow().maximize();
+        } else {
+            remote.getCurrentWindow().unmaximize();
+        }
+    }
+    function closeWindow() {
+        remote.getCurrentWindow().close();
+    }
+    let content = <></>
+    if (data.isConsole) {
+        return (
+            <header>
+                <nav>
+                    <Menu inverted size='mini'>
+                        <Menu.Item header>
+                            Era.js Console
+                        </Menu.Item>
+                        <Menu.Menu position='right'>
+                            <Menu.Item>-</Menu.Item>
+                            <Menu.Item>+</Menu.Item>
+                            <Menu.Item>×</Menu.Item>
+                        </Menu.Menu>
+                    </Menu>
+                </nav>
+            </header>
+        )
+    } else if (data.isMenu) {
+        return (
+            <header>
+                <nav>
+                    <Menu inverted size='mini'>
+                        <Menu.Item header>
+                            Era.js
+                        </Menu.Item>
+                        <Menu.Menu position='right'>
+                            <Menu.Item>-</Menu.Item>
+                            <Menu.Item>+</Menu.Item>
+                            <Menu.Item>×</Menu.Item>
+                        </Menu.Menu>
+                    </Menu>
+                </nav>
+            </header>
+        )
+    } else if (!data.isLoaded) {
+        return (
+            <header>
+                <nav>
+                    <Menu inverted size='mini'>
+                        <Menu.Item header>
+                            Era.js
+                        </Menu.Item>
+                        <Menu.Menu position='right'>
+                            <Menu.Item>-</Menu.Item>
+                            <Menu.Item>+</Menu.Item>
+                            <Menu.Item>×</Menu.Item>
+                        </Menu.Menu>
+                    </Menu>
+                </nav>
+            </header>
+        )
+    } else {
+        return (
+            <header>
+                <nav>
+                    <Menu inverted size='mini'>
+                        <Menu.Item header>
+                            Era.js
+                            </Menu.Item>
+                        <Dropdown item text='游戏'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>新建</Dropdown.Item>
+                                <Dropdown.Item>打开</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item>退出</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown item text='编辑'>
+                            <Dropdown.Menu>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown item text='窗口'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>游戏</Dropdown.Item>
+                                <Dropdown.Item>终端</Dropdown.Item>
+                                <Dropdown.Item>地图</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown item text='帮助'>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>帮助</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item>教程</Dropdown.Item>
+                                <Dropdown.Item>文档</Dropdown.Item>
+                                <Dropdown.Item>API</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item>检查更新</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item>关于</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Menu.Menu position='right'>
+                            <Menu.Item>-</Menu.Item>
+                            <Menu.Item>+</Menu.Item>
+                            <Menu.Item>×</Menu.Item>
+                        </Menu.Menu>
+                    </Menu>
+                </nav>
+            </header>
+        )
+    }
+}
+export function Toast(props: any) {
+    return (<></>)
+}

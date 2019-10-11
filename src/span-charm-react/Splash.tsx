193c6433a6770841b1798b2def11f3912c62c860
(ง •_•)ง <miswanting@users.noreply.github.com>
2019-10-11 22:17:15 +0800

    优化Splash界面生成逻辑

--- src/span-charm-react/Splash.tsx
+++ src/span-charm-react/Splash.tsx
@@ -1,27 +1,20 @@
 import React, { useState } from "react";
 export function Splash(props: any) {
+    let content = []
     if (props.data.load_text == '') {
-        return (
-            <div style={{ height: 100 + '%', display: 'flex', alignItems: 'center' }}>
-                <div style={{ width: 100 + '%', textAlign: 'center' }}>
-                    <div>{props.data.title}</div>
-                    <div>Connecting...</div>
-                    <div>按[Esc]键打开菜单</div>
-                    <div>按[`]键打开控制台</div>
-                </div>
-            </div>
-        )
+        content.push(<div>Connecting...</div>)
     } else {
-        return (
-            <div style={{ height: 100 + '%', display: 'flex', alignItems: 'center' }}>
-                <div style={{ width: 100 + '%', textAlign: 'center' }}>
-                    <div>{props.data.title}</div>
-                    <div>Loading...</div>
-                    <div>{props.data.load_text}</div>
-                    <div>按[Esc]键打开菜单</div>
-                    <div>按[`]键打开控制台</div>
-                </div>
-            </div>
-        )
+        content.push(<div>Loading...</div>)
+        content.push(<div>{props.data.load_text}</div>)
     }
+    return (
+        <main className="splash">
+            <div>
+                <div>{props.data.title}</div>
+                {content}
+                <div>按[Esc]键打开菜单</div>
+                <div>按[`]键打开控制台</div>
+            </div>
+        </main>
+    )
 }
